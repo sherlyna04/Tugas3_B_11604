e513a12f76f0a0fe6962faa00d446ec6394cf10a
sherlyna alfelia <220711604@students.uajy.ac.id>
2024-10-09 16:38:26 +0700

    Update Tugas_Modul3_B_SHERLYNA_11604.py

--- Tugas_Modul3_B_SHERLYNA_11604.py
+++ Tugas_Modul3_B_SHERLYNA_11604.py
@@ -71,8 +71,7 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload:</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'C:\Users\Sherlyna Alfelia\Downloads\Supervised Learning (Praktek)\Model Suprevised Learning (Praktek)\Tugas3_B_11604'
-    model_path = os.path.join(model_directory, r'SVR_IPK_model.pkl')
+    model_path = r'SVR_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
